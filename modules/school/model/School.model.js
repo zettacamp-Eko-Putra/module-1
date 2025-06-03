3ebb86461c7b11a30d5f4ce86edc3eedea778236
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-03 16:29:55 +0700

    adding school model

---
+++ modules/school/model/School.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// *************** model schema for school
+const schoolSchema = new Schema({
+
+    // name for the school
+    name : { type:String, required: true },
+
+    // address for the school
+    address : { type:String, required: true },
+
+    // Student for the school
+    student : { type:Schema.Types.ObjectId, ref:'Student' },
+
+    // delete at for the school
+    deletedAt : { type:Date, default:null }
+})
+
+// compile school schema into a model
+const School = mongoose.model('School',schoolSchema);
+
+// export model
+module.exports = School;
