0a3b32747e5ac2952f9e43352ec7cd31f0ebd2cc
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-03 17:37:21 +0700

    adding comment to connection to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.send('Server is running');
@@ -9,3 +10,8 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
+// *************** Connection to mongodb
+mongoose.connect('mongodb://localhost:27017/module')
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
