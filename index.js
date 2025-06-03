9917799f11b4f8957d2be90b61d06bd602f04b93
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-03 13:37:28 +0700

    reset #2

--- index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/module', {
-}).then(() => {
-  console.log("✅ MongoDB connected");
-}).catch((err) => {
-  console.error("❌ MongoDB connection error:", err);
-});
-
