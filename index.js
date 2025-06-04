4acad9e4c4609fad72aa47040323839769e0a8be
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-04 08:32:16 +0700

    adding comment to index.js and folder configuration

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+
+// *************** IMPORT CORE ***************
 const express = require('express');
-const app = express();
-const port = 3000;
 const mongoose = require('mongoose');
 
+// *************** Configuration ***************
+const app = express();
+const port = 3000;
+
+
+// *************** Connection express ***************
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+
+// *************** START: Server listening ***************
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
 
-// *************** Connection to mongodb
+// *************** END: server listening ***************
+
+
+// *************** Connection to mongodb ***************
 mongoose.connect('mongodb://localhost:27017/module')
   .then(() => console.log('✅ MongoDB connected'))
   .catch((err) => console.error('❌ MongoDB connection error:', err));
