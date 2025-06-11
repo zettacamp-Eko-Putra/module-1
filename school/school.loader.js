025cf336c3ebda493e3596adc448e2434c951f99
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-11 11:36:05 +0700

    adding comment to school loader and student loader

--- school/school.loader.js
+++ school/school.loader.js
@@ -1,15 +1,19 @@
+// *************** IMPORT MODULE *************** 
 const DataLoader = require('dataloader');
 const School = require('./school.models.js');
 const keyBy = require('lodash/keyBy');
 
+// *************** Batch School Data
 async function SchoolBatch(schoolId){
     const schools = await School.find({ _id: { $in: schoolId } });
     const schoolMap = keyBy(schools, school => school._id.toString());
     return schoolId.map(id => schoolMap[id.toString()]);
 }
 
+// *************** Adding Loader
 function CreateSchoolLoader(){
     return new DataLoader(SchoolBatch);
 }
 
+// *************** EXPORT MODULE *************** 
 module.exports = CreateSchoolLoader;
--- student/student.loader.js
+++ student/student.loader.js
@@ -3,6 +3,7 @@ const dataLoader = require('dataloader');
 const Student = require('./student.models.js');
 const keyBy = require('lodash/keyBy');
 
+// *************** Batch Student Data
 async function StudentBatch(studentId){
     const students = await Student.find({ 
         _id: { $in:studentId },
@@ -11,8 +12,10 @@ async function StudentBatch(studentId){
     return studentId.map(id => studentMap[id.toString()] || null);
 }
 
+// *************** Adding Loader
 const CreateStudentLoader = () => {
     return new dataLoader(StudentBatch)
 }
 
+// *************** EXPORT MODULE *************** 
 module.exports = CreateStudentLoader;
