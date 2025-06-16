9eaceec92c71d13dfbae9ea18ba85a5ba0fcda37
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-16 08:27:24 +0700

    improving school, student, user typedef

--- user/user.typedefs.js
+++ user/user.typedefs.js
@@ -1,84 +1,69 @@
 // *************** TYPE DEFINITION: User ***************
 
-// *************** User address Type
-const userAddressType = `
-type UserAddress{
-    street:String!
-    city:String!
-    province:String!
-    postal_code:String!
-}
-
-input UserAddressInput{
-    street:String!
-    city:String!
-    province:String!
-    postal_code:String!
-}
-`;
-
 // *************** User Type
-const userType = `
-type User {
-    _id:ID!
-    first_name:String!
-    last_name:String!
-    civility:String!
-    office_phone:String
-    direct_line:String
-    mobile_phone:String
-    entity:String!
-    address:[UserAddress]!
-    email:String!
-    password:String!
-    role:String!
-    status:String!
-    deleted_at:Date
-}
+const userTypeDefs = `
+    type UserAddress{
+        street:String!
+        city:String!
+        province:String!
+        postal_code:String!
+    }
 
-input UserInput{
-    _id:ID
-    first_name:String!
-    last_name:String!
-    civility:String!
-    office_phone:String
-    direct_line:String
-    mobile_phone:String!
-    entity:String!
-    address:[UserAddressInput]!
-    email:String!
-    password:String!
-    role:String! 
-}
-`;
+    input UserAddressInput{
+        street:String!
+        city:String!
+        province:String!
+        postal_code:String!
+    }
 
-// *************** User Query
-const userQuery = `
-extend type Query {
-    GetAllUsers: [User]
-    GetUserById(_id: ID!): User
-}
-`;
+    type User {
+        _id:ID!
+        first_name:String!
+        last_name:String!
+        civility:String!
+        office_phone:String
+        direct_line:String
+        mobile_phone:String
+        entity:String!
+        address:[UserAddress]!
+        email:String!
+        password:String!
+        role:String!
+        status:String!
+        deleted_at:Date
+    }
 
-// *************** User Mutation
-const userMutation = `
-extend type Mutation{
-    CreateUser(
-        user_input: UserInput!): User!    
+    input UserInput{
+        _id:ID
+        first_name:String!
+        last_name:String!
+        civility:String!
+        office_phone:String
+        direct_line:String
+        mobile_phone:String!
+        entity:String!
+        address:[UserAddressInput]!
+        email:String!
+        password:String!
+        role:String! 
+    }
     
-    UpdateUser(
-        _id:ID!,user_input: UserInput!): User
+    extend type Query {
+        GetAllUsers: [User]
+        GetUserById(_id: ID!): User
+    }
+
+    extend type Mutation{
+        CreateUser(
+            user_input: UserInput!): User!    
+    
+        UpdateUser(
+            _id:ID!,user_input: UserInput!): User
         
-    DeleteUser(
-        _id: ID!): User
+        DeleteUser(
+            _id: ID!): User
     }
 `;
 
-const userTypeDefs = `
-    ${userAddressType}
-    ${userType}    
-    ${userQuery}
-    ${userMutation}
-`;
 // *************** EXPORT MODULE ***************
 module.exports = userTypeDefs;
