36d0050ee8427f01b46863e1601a8d42e9f43b59
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-06-19 15:51:06 +0700

    [Module 1] Revise PR: improving school.validator.js based on feedback

--- modules/school/school.validator.js
+++ modules/school/school.validator.js
@@ -50,26 +50,25 @@ function ValidateSchoolInput(school_input) {
   if (!Array.isArray(school_input.address) || !school_input.address.length) {
     // *************** error message if the input not valid
     throw new ApolloError('Address is required.');
-  } else {
-    // *************** validate each address array
-    school_input.address.forEach((addr, index) => {
-      if (typeof addr.street !== 'string' || addr.street.trim() === '') {
-        throw new ApolloError(`Street at index ${index} not valid`);
-      }
-      if (typeof addr.city !== 'string' || addr.city.trim() === '') {
-        throw new ApolloError(`city at index ${index} not valid`);
-      }
-      if (typeof addr.province !== 'string' || addr.province.trim() === '') {
-        throw new ApolloError(`province at index ${index} not valid`);
-      }
-      if (
-        typeof addr.postal_code !== 'string' ||
-        addr.postal_code.trim() === ''
-      ) {
-        throw new ApolloError(`postal code at index ${index} not valid`);
-      }
-    });
   }
+  // *************** validate each address array
+  school_input.address.forEach((addr, index) => {
+    if (typeof addr.street !== 'string' || addr.street.trim() === '') {
+      throw new ApolloError(`Street at index ${index} not valid`);
+    }
+    if (typeof addr.city !== 'string' || addr.city.trim() === '') {
+      throw new ApolloError(`City at index ${index} not valid`);
+    }
+    if (typeof addr.province !== 'string' || addr.province.trim() === '') {
+      throw new ApolloError(`Province at index ${index} not valid`);
+    }
+    if (
+      typeof addr.postal_code !== 'string' ||
+      addr.postal_code.trim() === ''
+    ) {
+      throw new ApolloError(`Postal code at index ${index} not valid`);
+    }
+  });
 }
 
 // *************** EXPORT MODULE ***************
