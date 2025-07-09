3ad10a692fe74d833a1c514c79ff280ca9344441
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-07-09 08:49:29 +0700

    [module 2] PR: adding task typedef

---
+++ modules/test_management/task/task.typedefs.js
@@ -0,0 +1,57 @@
+// *************** IMPORT LIBRARY ***************
+const { gql } = require('apollo-server');
+
+// *************** Subject Task
+const taskTypeDefs = gql`
+  type Task {
+    _id: ID!
+    test_id: ID
+    user_id: ID
+    type: TypeTask!
+    task_status: TaskStatus!
+    status: Status
+    due_date: Date
+    created_at: Date
+    created_by: ID
+    updated_by: [UpdatedBy]
+    deleted_at: Date
+    deleted_by: ID
+  }
+
+  enum TypeTask {
+    ASSIGN_CORRECTOR
+    ENTER_MARKS
+    VALIDATE_MARKS
+  }
+
+  enum Status {
+    ACTIVE
+    DELETED
+  }
+
+  enum TaskStatus {
+    PENDING
+    IN_PROGRESS
+    COMPLETED
+  }
+
+  input TaskInput {
+    test_id: ID
+    user_id: ID
+    type: TypeTask!
+    due_date: Date
+  }
+
+  extend type Query {
+    GetAllTasks(type: TypeTask, task_status: TaskStatus): [Task]
+    GetOneTask(_id: ID): Task
+  }
+
+  extend type Mutation {
+    UpdateTask(_id: ID!, task_input: TaskInput!): Task
+    DeleteTask(_id: ID!): ID
+  }
+`;
+
+// *************** EXPORT MODULE ***************
+module.exports = taskTypeDefs;
