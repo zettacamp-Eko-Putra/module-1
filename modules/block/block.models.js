0d15aa81d6524f1290890d89b7a5cbc27fba4886
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-07-16 14:59:36 +0700

    [module 3] PR: adding aditional field

--- modules/block/block.models.js
+++ modules/block/block.models.js
@@ -17,6 +17,47 @@ const BlockSchema = new Schema(
     // Status for the block
     status: { type: String, enum: ['ACTIVE', 'DELETED'], default: 'ACTIVE' },
 
+    // Passing criteria for the block
+    passing_criteria: [
+      {
+        // logical operator for passing criteria
+        logical_operator: { type: String, enum: ['OR', 'AND'] },
+
+        // condition passing criteria for the block
+        condition: [
+          {
+            // enum condition outcome for the condition
+            condition_outcome: {
+              type: String,
+              enum: ['PASS', 'FAIL'],
+              required: true,
+            },
+            // condition type for block passing criteria condition
+            condition_type: {
+              type: String,
+              enum: ['SINGLE_SUBJECT', 'AVERAGE_MARK_SUBJECT', 'SINGLE_TEST'],
+              required: true,
+            },
+            // subject id for condition and type SINGLE_SUBJECT, AVERAGE_MARK_SUBJECT
+            subject_id: [{ type: Schema.Types.ObjectId, ref: 'subject' }],
+
+            // test id for condition and type SINGLE_TEST
+            test_id: [{ type: Schema.Types.ObjectId, ref: 'test' }],
+
+            // minimum mark for condition
+            min_mark: { type: Number, required: true },
+
+            // enum operator to compare with min_mark
+            operator: {
+              type: String,
+              enum: ['GREATER_THAN', 'GREATER_THAN_OR_EQUAL'],
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+
     // user id who create the block
     created_by: { type: Schema.Types.ObjectId, ref: 'user' },
 
