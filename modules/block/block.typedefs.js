b9eb07e29d2e4a2338b5bad0e16e61b42e160f79
zettacamp-Eko-Putra <eko.putra@zettacamp.pro>
2025-07-16 15:09:54 +0700

    [module 3] PR : adding typedef in block

--- modules/block/block.typedefs.js
+++ modules/block/block.typedefs.js
@@ -8,12 +8,43 @@ const blockTypeDefs = gql`
     name: String!
     description: String!
     subject_ids: [Subject]
+    passing_criteria: [PassingCriteria]
     status: BlockStatus!
     created_at: Date
     created_by: ID
     updated_by: [UpdatedBy]
     deleted_at: Date
     deleted_by: ID
+  }
+
+  type PassingCriteria {
+    logical_operator: [LogicalOperatorEnum]
+    condition: [BlockCondition]
+  }
+
+  type BlockCondition {
+    condition_type: ConditionTypeEnum
+    condition_outcome: ConditionOutcomeEnum
+    subject_id: [Subject]
+    test_id: [Test]
+    min_mark: Int
+    operator: OperatorEnum
+  }
+
+  enum OperatorEnum {
+    GREATER_THAN
+    GREATER_THAN_OR_EQUAL
+  }
+
+  enum ConditionTypeEnum {
+    SINGLE_SUBJECT
+    AVERAGE_MARK_SUBJECT
+    SINGLE_TEST
+  }
+
+  enum LogicalOperatorEnum {
+    OR
+    AND
   }
 
   enum BlockStatus {
